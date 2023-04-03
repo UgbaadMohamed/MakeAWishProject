adc117853b4f9347139946a3dc4d8e657c85f9c5
Ugbaad <ugbaad0012@gmail.com>
2023-04-03 15:20:30 +0200

    Initial commit

---
+++ src/main/java/com/example/makeawishproject/MakeAWishProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.makeawishproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeAWishProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MakeAWishProjectApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/makeawishproject/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.example.makeawishproject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class HomeController {
+    @GetMapping("/")
+    public String index(){
+
+        return "home/frontPage";
+    }
+
+}
