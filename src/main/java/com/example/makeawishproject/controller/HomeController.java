7f95e52d39ca836d8a08fc73409c3970fa9b64dc
Ugbaad <ugbaad0012@gmail.com>
2023-04-03 15:27:57 +0200

    hej

--- src/main/java/com/example/makeawishproject/controller/HomeController.java
+++ src/main/java/com/example/makeawishproject/controller/HomeController.java
@@ -1,11 +1,14 @@
 package com.example.makeawishproject.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
+@Controller
 public class HomeController {
     @GetMapping("/")
     public String index(){
-
+        System.out.println("hej");
         return "home/frontPage";
     }
 
