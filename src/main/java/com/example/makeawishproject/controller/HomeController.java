0c164e747a88a3d2935080c1d0a636efb9eac456
freyafuglo <frey3246@stud.kea.dk>
2023-04-03 15:29:13 +0200

    første commit

--- src/main/java/com/example/makeawishproject/controller/HomeController.java
+++ src/main/java/com/example/makeawishproject/controller/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     @GetMapping("/")
     public String index(){
-
+//Freyas controller
         return "home/frontPage";
     }
 
